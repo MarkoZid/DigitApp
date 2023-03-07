c91720fcf0947accad6931373a295808e45a7e6c
Drin <kadriudrin@gmail.com>
2023-03-07 16:01:16 +0100

    Implemented solid structure and split the app into 4 components: header, home-page, footer, planner

---
+++ src/app/trip-planner/trip-planner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-planner',
+  templateUrl: './trip-planner.component.html',
+  styleUrls: ['./trip-planner.component.scss']
+})
+export class TripPlannerComponent {
+
+}
